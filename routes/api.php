fc6d303b769033d842c81f5f2167ce5a08d52620
nepogabriel <nepogabriel16@gmail.com>
2025-09-05 22:48:47 -0300

    Corrigindo permissão do endpoint de buscar todos os clientes cadastrados

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\CustomerController;
 
 Route::middleware('checkToken:001')->post('/customers', [CustomerController::class, 'store']);
 Route::middleware('checkToken:002')->get('/customers/{id}', [CustomerController::class, 'show']);
-Route::middleware('checkToken:004')->get('/customers', [CustomerController::class, 'index']);
+Route::middleware('checkToken:003')->get('/customers', [CustomerController::class, 'index']);
