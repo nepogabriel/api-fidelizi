916cb9fb6ffe30214f7e93d41e851c85d5ea5fd9
nepogabriel <nepogabriel16@gmail.com>
2025-09-05 21:07:07 -0300

    Padronizando retorno do middleware com helper

--- app/Http/Middleware/CheckBearerToken.php
+++ app/Http/Middleware/CheckBearerToken.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\ApiResponse;
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,39 +15,39 @@ class CheckBearerToken
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $permission): Response
+    public function handle(Request $request, Closure $next, string $permission): JsonResponse|Response
     {
         try {
             $header = $request->header('Authorization');
 
             if (!$header || !preg_match('/Bearer\s+(\S+)/', $header, $matches)) {
-                return response()->json(['error' => 'Token não fornecido'], Response::HTTP_UNAUTHORIZED);
+                return ApiResponse::response(['error' => 'Token não fornecido'], Response::HTTP_UNAUTHORIZED);
             }
 
             $token = $matches[1];
             $tokens = config('tokens');
 
             if (!isset($tokens[$token])) {
-                return response()->json(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
+                return ApiResponse::response(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
             }
 
             if (!in_array($permission, $tokens[$token])) {
-                return response()->json(['error' => 'Permissão negada'], Response::HTTP_FORBIDDEN);
+                return ApiResponse::response(['error' => 'Permissão negada'], Response::HTTP_FORBIDDEN);
             }
         }
         catch (\InvalidArgumentException $e) {
-            return response()->json([
-                'error' => 'Invalid token format'
+            return ApiResponse::response([
+                'error' => 'Formato do token é inválido'
             ], Response::HTTP_BAD_REQUEST);
 
         } catch (\Exception $e) {
-            return response()->json([
+            return ApiResponse::response([
                 'error' => 'Unexpected exception',
                 'message' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
 
         } catch (\Throwable $e) {
-            return response()->json([
+            return ApiResponse::response([
                 'error' => 'Fatal error',
                 'message' => $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
